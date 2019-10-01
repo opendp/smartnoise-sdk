f4991cb1f4f47379dbafa1b4dfe70eea7be6816f
Eduardo de Leon <eddeleon@microsoft.com>
2019-10-01 19:35:21 -0400

    Add csv details with test

--- sdk/burdock/restclient/models/dataset_document.py
+++ sdk/burdock/restclient/models/dataset_document.py
@@ -13,12 +13,16 @@ class DatasetDocument(Model):
 
     :param dataset_type: The type of the dataset
     :type dataset_type: str
+    :param csv_details:
+    :type csv_details: ~restclient.models.LocalCSVDetails
     """
 
     _attribute_map = {
         'dataset_type': {'key': 'dataset_type', 'type': 'str'},
+        'csv_details': {'key': 'csv_details', 'type': 'LocalCSVDetails'},
     }
 
-    def __init__(self, dataset_type=None):
+    def __init__(self, dataset_type=None, csv_details=None):
         super(DatasetDocument, self).__init__()
         self.dataset_type = dataset_type
+        self.csv_details = csv_details
--- tests/service/dataset/test_smoke.py
+++ tests/service/dataset/test_smoke.py
@@ -5,7 +5,12 @@ import pytest
 from burdock.restclient.models.dataset_read_request import DatasetReadRequest
 
 @pytest.mark.parametrize("dataset_id", ["0"])
-def test_execute_run(client, dataset_id):
+def test_read_local_csv(client, dataset_id):
     read_request = DatasetReadRequest(dataset_id=dataset_id)
-    client.datasetread(read_request)
+    dataset_document = client.datasetread(read_request)
+    local_csv_details = dataset_document.csv_details
+
+    with open(local_csv_details.local_path, "r") as stream:
+        text = stream.read()
+        assert len(text) > 0
 
