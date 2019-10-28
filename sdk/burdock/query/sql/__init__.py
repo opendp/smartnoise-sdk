9bdc801e1d4bf8ba548cd175d0a10fb934aa12e6
Eduardo de Leon <eddeleon@microsoft.com>
2019-10-27 20:45:21 -0400

    Add rewriter base

--- sdk/burdock/query/sql/__init__.py
+++ sdk/burdock/query/sql/__init__.py
@@ -1,4 +1,5 @@
 from .metadata.config_file import MetadataLoader
 from .ast.parse import QueryParser
+from .private.rewrite import Rewriter
 
-__all__ = ["MetadataLoader", "QueryParser"]
+__all__ = ["MetadataLoader", "QueryParser", "Rewriter"]
