3772c1534b711a0d843c8c56ad73b032d5cf6607
Eduardo de Leon <eddeleon@microsoft.com>
2019-11-17 17:45:18 -0500

    Fix bug in params for sql test

--- tests/service/execute/test_smoke.py
+++ tests/service/execute/test_smoke.py
@@ -1,10 +1,9 @@
-import json
-
 import pytest
 
 @pytest.mark.parametrize("project", [{"params": {"text": "this text"}, "uri": "modules/module-example"},
-                                     {"dataset_name": "example", "budget": .2,
+                                     {"params": {"dataset_name": "example", "budget": .2,
                                       "query": "SELECT COUNT(*) FROM example.example"},
+                                      "uri": "modules/sql-module"},
                                      {"params": {"dataset_name": "example", "column_name": "a", "budget": .2},
                                       "uri": "modules/psi-count-module"}])
 def test_execute_run(execution_client, project):
