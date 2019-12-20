6d9ab1a0f3932686c0ea6a46eeacb4d9bf86916c
Joshua Allen <55251797+joshua-oss@users.noreply.github.com>
2019-12-20 11:49:51 -0800

    Fixes to mechanisms

--- sdk/burdock/mechanisms/laplace.py
+++ sdk/burdock/mechanisms/laplace.py
@@ -1,20 +1,21 @@
 import numpy as np
+from scipy.stats import laplace
 
 
 class Laplace:
-    def __init__(self, epsilon, tau=5):
+    def __init__(self, epsilon, tau=1):
         self.epsilon = epsilon
         self.tau = tau
 
     def count(self, vals):
-        noise = np.random.normal(0.0, self.tau / self.epsilon, len(vals))
+        noise = np.random.laplace(0.0, self.tau / self.epsilon, len(vals))
         new_vals = noise + vals
         return new_vals
 
     def sum_int(self, vals, sensitivity):
-        noise = np.random.laplace(0.0, sensitivity / self.epsilon, len(vals))
+        noise = np.random.laplace(0.0, (self.tau * sensitivity) / self.epsilon, len(vals))
         return np.array(noise).astype(int) + vals
 
     def sum_float(self, vals, sensitivity):
-        noise = np.random.normal(0.0, sensitivity / self.epsilon, len(vals))
+        noise = np.random.laplace(0.0, (self.tau * sensitivity) / self.epsilon, len(vals))
         return noise + vals
