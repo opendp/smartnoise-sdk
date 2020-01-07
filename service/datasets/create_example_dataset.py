13f959d42695b25cd854c038d4bde537fad745ff
Maya Nigrin <36938499+mayigrin@users.noreply.github.com>
2020-01-07 15:11:51 -0500

    Wrote code to create new example dataset, overwrote example.csv (#68)
    * Wrote code to create new example dataset, used that to overwrite example.csv
    
    * Made example dataset larger

---
+++ service/datasets/create_example_dataset.py
@@ -0,0 +1,20 @@
+import random
+
+"""
+Code to create an example dataset for testing. c is y-value; 
+c is 0 if a is low and is high, and is 1 otherwise
+"""
+
+list_a = [random.randint(0, 50) for i in range(1000)]
+list_b = [random.randint(0, 50) for i in range(1000)]
+f = open("example.csv", "w", newline='')
+f.write("a,b,c\n")
+for i in range(len(list_a)):
+    a = list_a[i]
+    b = list_b[i]
+    if a < 25 < b:
+        c = 0
+    else:
+        c = 1
+    f.write(str(a)+","+str(b)+","+str(c)+"\n")
+f.close()
