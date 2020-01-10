6a3c02ed3a6640f9942d1198240c02963943d7dd
Joshua Allen <55251797+joshua-oss@users.noreply.github.com>
2020-01-10 09:37:14 -0800

    Release metadata

--- sdk/burdock/query/sql/metadata/release.py
+++ sdk/burdock/query/sql/metadata/release.py
@@ -6,21 +6,19 @@ will be updated to align with yarrow after yarrow enhanced release
 metadata format is specified.
 """
 class Result:
-    """
-    Instantiate a release.
-
-    Parameters:
-        statistic (string): The label for the statistic being computed (e.g. 'sum', 'mean')
-        mechanism (string): The label for the mechanism being used (e.g. 'laplace', 'gaussian')
-        name (string): The name for the variable, usually the column name
-        epsilon (float): The epsilon that was used in computing this value
-        delta (float): The delta privacy parameter used
-        release (string[]): Vector of noisy release values as string
-        intervals ((pct, float, float)[]): Lower and upper bounds of confidence intervals at each pct.
-            By default, only 95% is returned, but caller can request multiple.  The interval
-            returned is centered on 0, and should not be thought of as an error bound.
-    """
     def __init__(self, statistic, mechanism, name, epsilon, delta, release, intervals):
+        """Instantiate a release.
+
+        :param string statistic: The label for the statistic being computed (e.g. 'sum', 'mean')
+        :param string mechanism: The label for the mechanism being used (e.g. 'laplace', 'gaussian')
+        :param string name: The name for the variable, usually the column name
+        :param float epsilon: The epsilon that was used in computing this value
+        :param float delta: The delta privacy parameter used
+        :param string[] release: Vector of noisy release values as string
+        :param ((float, float, float)[]) intervals: Lower and upper bounds of confidence intervals at each pct.
+         By default, only 95% is returned, but caller can request multiple.  The interval
+         returned is centered on 0, and should not be thought of as an error bound.
+        """
         self.statistic = statistic
         self.mechanism = mechanism
         self.name = name
