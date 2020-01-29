854155274456be9dea11a665c5fd7ad77215fc3e
Joshua Allen <55251797+joshua-oss@users.noreply.github.com>
2020-01-28 16:01:13 -0800

    Use object literal for metadata

--- tests/test_local_analyst.py
+++ tests/test_local_analyst.py
@@ -2,40 +2,23 @@
 import sklearn.datasets
 import pandas as pd
 
-from burdock.query.sql import DataFrameReader, MetadataLoader, execute_private_query
+from burdock.query.sql import DataFrameReader, execute_private_query
+from burdock.query.sql.metadata.metadata import Database, Table, Float
 
 def test_sklearn_query():
-    sklearn_dataset = sklearn.datasets.load_iris()
-    sklearn_df = pd.DataFrame(data=sklearn_dataset.data, columns=sklearn_dataset.feature_names)
+   sklearn_dataset = sklearn.datasets.load_iris()
+   sklearn_df = pd.DataFrame(data=sklearn_dataset.data, columns=sklearn_dataset.feature_names)
 
-    schema_dict = {"Database": {
-                     "dbo": {
-                       "iris": {
-                         "rows": 150,
-                         "sepal length (cm)": {
-                            "type": "float",
-                            "lower": 4,
-                            "upper": 8},
-                         "sepal width (cm)": {
-                            "type": "float",
-                            "lower": 2,
-                            "upper": 5},
-                         "petal length (cm)": {
-                            "type": "float",
-                            "lower": 1,
-                            "upper": 7},
-                         "petal width (cm)": {
-                            "type": "float",
-                            "lower": 0,
-                            "upper": 3}
-                        }
-                      }
-                    }
-                  }
+   iris = Table("dbo", "iris", 150, [
+      Float("sepal length (cm)", 4, 8),
+      Float("sepal width (cm)", 2, 5),
+      Float("petal length (cm)", 1, 7),
+      Float("petal width (cm)", 0, 3)
+   ])
+   schema = Database([iris], "csv")
 
-    schema = MetadataLoader.from_dict(schema_dict)
-    reader = DataFrameReader(schema, sklearn_df)
-    rowset = execute_private_query(reader, schema, 0.3, 'SELECT AVG("petal width (cm)") FROM dbo.iris')
-    df = pd.DataFrame(rowset[1:], columns=rowset[0])
-    assert df is not None
-    assert len(df) == 1
+   reader = DataFrameReader(schema, sklearn_df)
+   rowset = execute_private_query(reader, schema, 0.3, 'SELECT AVG("petal width (cm)") FROM dbo.iris')
+   df = pd.DataFrame(rowset[1:], columns=rowset[0])
+   assert df is not None
+   assert len(df) == 1
