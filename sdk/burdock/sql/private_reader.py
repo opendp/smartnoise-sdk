da3f808dc2ea8534f192c720c3a259fd4319caa9
Joshua Allen <55251797+joshua-oss@users.noreply.github.com>
2020-02-26 14:55:00 -0800

    cleanup

--- sdk/burdock/sql/private_reader.py
+++ sdk/burdock/sql/private_reader.py
@@ -147,7 +147,6 @@ class PrivateReader:
                 break
         if interval_widths is not None:
             for confidence in interval_widths:
-                print("looking at range: {0}".format(confidence))
                 bind_low = {}
                 bind_high = {}
                 for name in db_rsc.keys():
