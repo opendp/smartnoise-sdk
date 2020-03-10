1339e1f41e070ec26a85eff65e4dfa5394deb6cb
Joshua Allen <55251797+joshua-oss@users.noreply.github.com>
2020-03-10 11:18:47 -0700

    Spark already returns typed result

--- sdk/burdock/reader/sql/spark.py
+++ sdk/burdock/reader/sql/spark.py
@@ -23,6 +23,9 @@ class SparkReader(SqlReader):
 
         df = self.api.sql(query)
         return df
+
+    def execute_typed(self, query):
+        return self.execute(query)
         
     def db_name(self):
         return self.database
