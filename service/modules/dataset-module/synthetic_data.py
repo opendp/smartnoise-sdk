337206b975ff696eda23026288b50ce836f99f0d
Lucas Rosenblatt <lurosenb@microsoft.com>
2020-03-18 23:39:29 -0400

    Synthetic data module started.

--- service/modules/dataset-module/synthetic_data.py
+++ service/modules/dataset-module/synthetic_data.py
@@ -1,30 +1,42 @@
 import mlflow
 import json
 import sys
+import os
 
 import pandas as pd
 
-from burdock.client import get_dataset_client
-from burdock.data.adapters import load_reader, load_metadata, load_dataset
-from burdock.sql.private_reader import PrivateReader
+from opendp.whitenoise.client import get_dataset_client
+from opendp.whitenoise.data.adapters import load_reader, load_metadata, load_dataset
+from opendp.whitenoise.sql.private_reader import PrivateReader
 from pandasql import sqldf
 
 
 if __name__ == "__main__":
     dataset_name = sys.argv[1]
-    budget = float(sys.argv[2])
-    query = sys.argv[3]
+    dataset_type = sys.argv[2]
+    budget = sys.argv[3]
+    json_flag = sys.argv[4]
+
+    details = {}
+
+    if json_flag:
+        # Dump the json
+        pass
+    else:
+        for arg in sys.argv[5:]:
+            details[arg.split("=")[0]] = arg.split("=")[1]
 
     with mlflow.start_run():
-        dataset_document = get_dataset_client().read(dataset_name, budget)
-        dataset = load_dataset(dataset_document)
-        reader = load_reader(dataset_document)
-        schema = load_metadata(dataset_document)
-        private_reader = PrivateReader(reader, schema, budget)
-        rowset = private_reader.execute(query)
+        # Optionally load the schema from a json? 
+        new_dataset = {
+            "dataset_name": dataset_name,
+            "dataset_type": dataset_type,
+            dataset_type: details,
+            "budget": budget
+        }
 
-        result = {"query_result": rowset}
-        df = pd.DataFrame(rowset[1:], columns=rowset[0])
-        with open("result.json", "w") as stream:
-            json.dump(df.to_dict(), stream)
-        mlflow.log_artifact("result.json")
+        dataset_document = get_dataset_client().register(new_dataset)
+
+        # with open("result.json", "w") as stream:
+        #     json.dump(df.to_dict(), stream)
+        # mlflow.log_artifact("result.json")
