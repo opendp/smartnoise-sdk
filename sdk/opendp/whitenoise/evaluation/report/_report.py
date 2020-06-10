b42675ac68929f88ab1d2395f703d9522e8f79f2
Ankit Srivastava <ankit.ufl@gmail.com>
2020-06-10 17:50:05 +0530

    Updating schema of report

--- sdk/opendp/whitenoise/evaluation/report/_report.py
+++ sdk/opendp/whitenoise/evaluation/report/_report.py
@@ -1,12 +1,19 @@
+import pandas as pd
+
 class Report:
 	"""
-	Defines the fields used to set evaluation parameters
-	and consumed by the evaluator
+	Defines the consistent schema of reported fields
+	that aid evaluation of a black box DP implementation
+	* res_df: It is a dataframe that contains repeated 
+	analysis results across dimension and numerical
+	columns. It could be exact or noisy based on the 
+	parameter actual = False or True in analysis
+	* dim_cols: List of columns that contain dimension
+	strings
+	* num_cols: List of columns that contain numerical
+	DP results
 	"""
 	def __init__(self):
-		self.noisy_vals = []
-		self.exact_val = 0.0
-		self.low = []
-		self.high = []
+		self.res_df = pd.DataFrame()
 		self.dim_cols = []
 		self.num_cols = []
