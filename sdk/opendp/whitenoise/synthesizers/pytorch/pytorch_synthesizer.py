c0a5c8695d70883f8d27d8912dcfbd2878cea42c
Xiaoyan Liu <xliu730@gatech.edu>
2020-07-15 17:27:54 -0700

    added scenario when no processor is needed and check data types for generated data

--- sdk/opendp/whitenoise/synthesizers/pytorch/pytorch_synthesizer.py
+++ sdk/opendp/whitenoise/synthesizers/pytorch/pytorch_synthesizer.py
@@ -11,19 +11,28 @@ class PytorchDPSynthesizer(SDGYMBaseSynthesizer):
 
         self.categorical_columns = None
         self.ordinal_columns = None
+        self.dtypes = None
     
     def fit(self, data, categorical_columns=tuple(), ordinal_columns=tuple()):
         self.categorical_columns = categorical_columns
         self.ordinal_columns = ordinal_columns
-        
-        self.preprocessor.fit(data, categorical_columns, ordinal_columns)
-        preprocessed_data = self.preprocessor.transform(data)
+        self.dtypes = data.dtypes
 
-        self.gan.train(preprocessed_data)
+        if self.preprocessor:
+            self.preprocessor.fit(data, categorical_columns, ordinal_columns)
+            data = self.preprocessor.transform(data)
+
+        self.gan.train(data)
     
     def sample(self, n):
         synth_data = self.gan.generate(n)
         
+        if not self.preprocessor:
+            if sum(synth_data.dtypes!=self.dtypes) > 0 :
+                convert_dict = self.dtypes.to_dict()
+                synth_data = synth_data.astype(convert_dict)
+            return synth_data
+
         if isinstance(self.preprocessor, GeneralTransformer):
             synth_data = self.preprocessor.inverse_transform(synth_data, None)
         else:
