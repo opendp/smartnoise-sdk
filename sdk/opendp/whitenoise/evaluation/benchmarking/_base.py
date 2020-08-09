90172ed5c93f1b77cf586ee32168e765b11feb40
Ankit Srivastava <ankit.ufl@gmail.com>
2020-08-09 19:33:35 +0530

    Initial stub of DP benchmarking

---
+++ sdk/opendp/whitenoise/evaluation/benchmarking/_base.py
@@ -0,0 +1,32 @@
+from opendp.whitenoise.evaluation.privacyalgorithm._base import PrivacyAlgorithm
+from opendp.whitenoise.evaluation.params._privacy_params import PrivacyParams
+from opendp.whitenoise.evaluation.params._eval_params import EvaluatorParams
+from opendp.whitenoise.evaluation.metrics._metrics import Metrics
+from abc import ABC, abstractmethod
+
+class Benchmarking(ABC):
+	"""
+	Interface for various DP implementations to interface with tests available
+	in evaluator. Evaluator tests for various properties of DP implementation
+	like privacy, accuracy, utility and bias
+	"""
+	@abstractmethod
+	def benchmark(self, 
+		d1 : object, 
+		d2 : object, 
+		pa : PrivacyAlgorithm,
+		algorithm : object, 
+		privacy_params : PrivacyParams, 
+		eval_params : EvaluatorParams) -> {[PrivacyParams, EvaluatorParams] : Metrics}:
+		"""
+		Benchmarks properties of privacy algorithm DP implementations using metrics
+			- Privacy Promise
+			- Accuracy Promise
+			- Utility Promise
+			- Bias Promise
+		
+		d1 and d2 are neighboring datasets
+		algorithm is the DP implementation object
+		Returns a metrics object
+		"""
+		pass
