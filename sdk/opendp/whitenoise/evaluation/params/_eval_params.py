3434f8178234d1284093070f58c21a80d6866139
Ankit Srivastava <ankit.ufl@gmail.com>
2020-08-12 13:50:24 +0530

    Setting default Eval params

--- sdk/opendp/whitenoise/evaluation/params/_eval_params.py
+++ sdk/opendp/whitenoise/evaluation/params/_eval_params.py
@@ -3,11 +3,19 @@ class EvaluatorParams:
 	Defines the fields used to set evaluation parameters
     and consumed by the evaluator
 	"""
-	def __init__(self, repeat_count=500, numbins=0):
+	def __init__(self, 
+			repeat_count=500, 
+			numbins=0, 
+			binsize="auto", 
+			exact=False,
+			alpha=0.05,
+			bound = True,
+			eval_first_key = False
+		):
 		self.repeat_count = repeat_count
 		self.numbins = numbins
-		self.binsize="auto"
-		self.exact=False
-		self.alpha=0.05
-		self.bound = True
-		self.eval_first_key = False
+		self.binsize = binsize
+		self.exact = exact
+		self.alpha = alpha
+		self.bound = bound
+		self.eval_first_key = eval_first_key
