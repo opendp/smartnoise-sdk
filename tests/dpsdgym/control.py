49dde6740b4866dc4c0d306a734432e88f20701d
eedeleon <31962564+eedeleon@users.noreply.github.com>
2020-09-17 11:16:19 -0400

    Update control.py

--- tests/dpsdgym/control.py
+++ tests/dpsdgym/control.py
@@ -1,7 +1,7 @@
 import mlflow
 from conf import KNOWN_DATASETS
 
-mlflow.set_experiment("smartnoise_synth_data_eval")
+mlflow.set_experiment("whitenoise_synth_data_eval")
 for i in range(10):
     for dataset in KNOWN_DATASETS:
         mlflow.projects.run(".", parameters={"dataset": dataset},
