ee9308991c6875d13e6180f6e6890999dffa9d24
Sha Li <shali@microsoft.com>
2020-11-20 15:28:12 -0800

    update test rl

--- tests/sdk/evaluation/test_bandit.py
+++ tests/sdk/evaluation/test_bandit.py
@@ -15,32 +15,18 @@ from opendp.smartnoise.evaluation.params._dataset_params import DatasetParams
 from opendp.smartnoise.evaluation.evaluator._dp_evaluator import DPEvaluator
 from opendp.smartnoise.sql import PandasReader
 from dp_singleton_query import DPSingletonQuery 
+from opendp.smartnoise.evaluation.learner.bandit import Bandit
+from opendp.smartnoise.evaluation.learner.util import generate_query
 
-
-class TestBandit():
-    def setup(self):
-        self.pp = PrivacyParams(epsilon=1.0)
-        self.ev = EvaluatorParams(repeat_count=100)
-        self.dd = DatasetParams(dataset_size=500)
-        self.pa = DPSingletonQuery()
-
-
-    def bandit(self, query):
-        df, metadata = create_simulated_dataset(self.dd.dataset_size, "dataset")
-        d1_dataset, d2_dataset, d1_metadata, d2_metadata = generate_neighbors(df, metadata)
-        d1 = PandasReader(d1_dataset, d1_metadata)
-        d2 = PandasReader(d2_dataset, d2_metadata)
-        eval = DPEvaluator()
-        pa = DPSingletonQuery()
-        key_metrics = eval.evaluate([d1_metadata, d1], [d2_metadata, d2], pa, query, self.pp, self.ev)
-        for key, metrics in key_metrics.items():
-            dp_res = metrics.dp_res
-            js_res = metrics.jensen_shannon_divergence
-            assert(metrics.dp_res == True)
-            assert(metrics.jensen_shannon_divergence > 0.0)
-            test_logger.debug("Wasserstein Distance:" + str(metrics.wasserstein_distance))
-            test_logger.debug("Jensen Shannon Divergence:" + str(metrics.jensen_shannon_divergence))
-
+class TestBandit():  
     def test_bandit(self):
-        query= "SELECT COUNT(UserId) AS UserCount FROM dataset.dataset"
-        self.bandit(query)
+        b = Bandit(PrivacyParams, EvaluatorParams, DatasetParams)
+        querypool = generate_query(3)
+        output = b.bandit(["SELECT COUNT(UserId) AS UserCount FROM dataset.dataset"] + querypool)
+        assert(output[0]['dpresult'] == True)
+        assert(output[0]['jensen_shannon_divergence'] > 0.0)
+
+
+
+
+
--- tests/sdk/evaluation/test_rl.py
+++ tests/sdk/evaluation/test_rl.py
@@ -10,49 +10,16 @@ from opendp.smartnoise.evaluation.params._dataset_params import DatasetParams
 from opendp.smartnoise.evaluation.learner._dp_env import DPEnv
 from opendp.smartnoise.evaluation.learner._generate import Grammar
 from opendp.smartnoise.evaluation.learner._computeactions import compute_action
-
+from opendp.smartnoise.evaluation.learner.util import create_simulated_dataset, generate_neighbors
+from opendp.smartnoise.evaluation.learner.Qlearning import QLearning
+from opendp.smartnoise.evaluation.learner.util import generate_query
 
 class TestQlearning():
-    def setup(self):
-        self.lp = LearnerParams(observation_space=30000, num_episodes=200, num_steps=200)
-        self.pp = PrivacyParams(epsilon=1.0)
-        self.ev = EvaluatorParams(repeat_count=100)
-        self.dd = DatasetParams(dataset_size=500)
-
-    def qlearning(self, querypool): 
-        available_actions = compute_action(self.lp)
-        env = DPEnv(self.lp, self.pp, self.ev, self.dd, querypool, available_actions)
-        # Set learning parameters
-        eps = self.lp.eps
-        lr = self.lp.lr
-        y = self.lp.y
-        num_episodes = 1
-        num_steps = 2
-        #Initialize table with all zeros
-        Q = np.zeros([env.observation_space.n,env.action_space.n])
-        for i in range(num_episodes):            
-            logging.debug("%s episode" %i)
-            #Reset environment and get first new observation
-            s = env.reset()
-            env.episode = i
-            d = False
-            j = 0
-            while j < num_steps:
-                j+=1
-                #Choose an action by greedily (with noise) picking from Q table
-                if np.random.random() < eps: # explore
-                    a = np.random.randint(env.action_space.n)
-                else:
-                    a = np.argmax((Q[s,:] + np.random.randn(1,env.action_space.n)*(1./(i+1)))) 
-                #Get new state and reward from environment
-                s1,r,d,info = env.step(a)
-                #Update Q-Table with new knowledge
-                Q[s,a] = Q[s,a] + lr*(r + y*np.max(Q[s1,:]) - Q[s,a])
-                s = s1
-                if d == True:
-                    break
-                assert((info['dpresult'] == 'DP_PASS') | (info['dpresult'] == 'ActionResultedSameQuery') | (info['dpresult'] == 'DP_BUG'))
-
     def test_qlearning(self):
-        querypool = ["SELECT COUNT(UserId) AS UserCount FROM dataset.dataset"]
-        self.qlearning(querypool)
+        lp = LearnerParams(observation_space=30000, num_episodes=2, num_steps=2)
+        b = QLearning(lp, PrivacyParams, EvaluatorParams, DatasetParams)
+        querypool = generate_query(2)
+        b.qlearning(["SELECT COUNT(UserId) AS UserCount FROM dataset.dataset"] + querypool)
+        output = b.qlearning(querypool)
+        assert((output[0]['dpresult'] == 'DP_PASS') | (output[0]['dpresult'] == 'ActionResultedSameQuery') | (output[0]['dpresult'] == 'DP_BUG'))
+
