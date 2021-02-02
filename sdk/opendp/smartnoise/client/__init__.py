4a97a379945f501947c16f3cedfa64a9213b66ad
Eduardo de Leon <eddeleon@microsoft.com>
2021-02-02 11:55:38 -0500

    Add client formatting

--- sdk/opendp/smartnoise/client/__init__.py
+++ sdk/opendp/smartnoise/client/__init__.py
@@ -16,7 +16,6 @@ module_logger = logging.getLogger(__name__)
 
 KNOWN_DATASET_TYPE_KEYS = ["csv_details", "dataverse_details"]
 
-
 class _MockCredentials(object):
     def signed_session(self, session=None):
         return session if session is not None else Session()
@@ -31,7 +30,6 @@ def _get_client():
     client = RestClient(_MockCredentials(), base_url)
     return client
 
-
 def _guid_header():
     """
     Generates a guid header for client, to be tracked by client/server.
@@ -41,24 +39,22 @@ def _guid_header():
     """
     return str(uuid.uuid4())
 
-
 class ExecutionClient(object):
     def submit(self, params, uri):
         client = _get_client()
-        details = ProjectRunDetails(params=json.dumps(params), project_uri=uri)
+        details = ProjectRunDetails(params=json.dumps(params),
+                                    project_uri=uri)
         return client.executerun(details)
-
 
 class DatasetClient(object):
     """
     A client for registering, reading and releasing differentially private
     datasets using the opendp-smartnoise service
     """
-
     def __init__(self):
         # Tag requests with this custom header for now
         self._guid = _guid_header()
-        self.custom_headers = {"client_guid": self._guid}
+        self.custom_headers = {'client_guid': self._guid}
 
     def _register_release_request_helper(self, dataset):
         """
@@ -67,17 +63,15 @@ class DatasetClient(object):
         """
         for key in KNOWN_DATASET_TYPE_KEYS:
             if not key in dataset:
-                dataset[key] = None
+                dataset[key]=None
 
-        request = DatasetDocument(
-            dataset_name=dataset["dataset_name"],
-            dataset_type=dataset["dataset_type"],
-            budget=dataset["budget"],
-            release_cost=dataset["release_cost"],
-            csv_details=dataset["csv_details"],
-            dataverse_details=dataset["dataverse_details"],
-            authorized_users=dataset["authorized_users"],
-        )
+        request = DatasetDocument(dataset_name=dataset['dataset_name'], \
+            dataset_type=dataset['dataset_type'], \
+            budget=dataset['budget'], \
+            release_cost=dataset['release_cost'], \
+            csv_details=dataset['csv_details'], \
+            dataverse_details=dataset['dataverse_details'], \
+            authorized_users=dataset['authorized_users'])
 
         return request
 
@@ -121,25 +115,21 @@ class DatasetClient(object):
         read_released_request = DatasetReadReleaseRequest(dataset_name=dataset_name)
         return client.datasetreadreleased(read_released_request, custom_headers=self.custom_headers)
 
-
 def get_dataset_client():
-    client_overrides = [
-        entrypoint
-        for entrypoint in pkg_resources.iter_entry_points("opendp_smartnoise_dataset_client")
-    ]
+    client_overrides = [entrypoint for entrypoint in pkg_resources.iter_entry_points("opendp_smartnoise_dataset_client")]
     if len(client_overrides) == 1:
         try:
             entrypoint = client_overrides[0]
             extension_class = entrypoint.load()
             return extension_class()
         except Exception as e:  # pragma: no cover
-            msg = "Failure while loading {} with exception {}.".format(entrypoint, e)
-            module_logger.warning(msg)
+                msg = "Failure while loading {} with exception {}.".format(
+                    entrypoint, e)
+                module_logger.warning(msg)
     else:
         if len(client_overrides) > 1:
-            module_logger.warning("Multiple client overrides found {}".format(client_overrides))
+                module_logger.warning("Multiple client overrides found {}".format(client_overrides))
     return DatasetClient()
-
 
 def get_execution_client():
     return ExecutionClient()
