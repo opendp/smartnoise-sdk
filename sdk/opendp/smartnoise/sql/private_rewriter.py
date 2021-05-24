93beb91c2f6df69a43bd5ac2ce2c8973be7d5623
Victoria de Sainte Agathe <victoria.desainteagathe@gmail.com>
2021-05-25 00:52:51 +0200

    Do not clamp grouping columns (#360)
    * no clamp for grouping cols
    
    * typo
    
    * fix error
    
    * rm print
    
    * Add check for missing maxval
    
    * Unit tests for grouping clamp fix
    
    Co-authored-by: Victoria de Sainte Agathe <vdsa@sarus.tech>
    Co-authored-by: Joshua Allen <55251797+joshua-oss@users.noreply.github.com>

--- sdk/opendp/smartnoise/sql/private_rewriter.py
+++ sdk/opendp/smartnoise/sql/private_rewriter.py
@@ -247,44 +247,53 @@ class Rewriter:
         child_scope = Scope()
         relations = query.source.relations
         select = [
-                self.clamp_expression(ne, relations, child_scope, self.options.clamp_columns)
+                self.clamp_expression(ne, relations, child_scope, query, self.options.clamp_columns)
                 for ne in query.select.namedExpressions
             ]
         select = Select(None, select)
         subquery = Query(child_scope.select(), query.source, query.where, None, None, None, None)
         return subquery
 
-    def clamp_expression(self, ne, relations, scope, do_clamp=True):
+    def clamp_expression(self, ne, relations, scope, query, do_clamp=True):
         """
             Lookup the expression referenced in each named expression and
             write a clamped select for it, using the schema
         """
+        def bounds_clamp(colname):
+            if not do_clamp:
+                return None, None
+            if query.agg:
+                grouping_colnames = [col.name for col in query.agg.groupedColumns()]
+                if colname in grouping_colnames:
+                    return None, None
+            minval = None
+            maxval = None
+            sym = col.symbol(relations)
+            if sym.valtype in ["float", "int"] and not sym.unbounded:
+                minval = sym.minval
+                maxval = sym.maxval
+            if minval is None or maxval is None or sym.is_key:
+                return None, None
+            return minval, maxval
+
         exp = ne.expression
         cols = exp.find_nodes(Column)
         if type(exp) is Column:
             cols += [exp]
         for col in cols:
             colname = col.name
-            minval = None
-            maxval = None
-            sym = col.symbol(relations)
-            if do_clamp and sym.valtype in ["float", "int"] and not sym.unbounded:
-                minval = sym.minval
-                maxval = sym.maxval
-                if minval is None or sym.is_key:
-                    cexpr = Column(colname)
-                    ce_name = scope.push_name(cexpr, str(colname))
-                else:
-                    when_min = WhenExpression(
-                        BooleanCompare(col, Op("<"), Literal(minval)), Literal(minval)
-                        )
-                    when_max = WhenExpression(
-                        BooleanCompare(col, Op(">"), Literal(maxval)), Literal(maxval)
-                        )
-                    cexpr = CaseExpression(None, [when_min, when_max], col)
-                    ce_name = scope.push_name(cexpr, str(colname))
-            else:
+            minval, maxval = bounds_clamp(colname)
+            if minval == None:
                 cexpr = Column(colname)
+                ce_name = scope.push_name(cexpr, str(colname))
+            else:
+                when_min = WhenExpression(
+                    BooleanCompare(col, Op("<"), Literal(minval)), Literal(minval)
+                    )
+                when_max = WhenExpression(
+                    BooleanCompare(col, Op(">"), Literal(maxval)), Literal(maxval)
+                    )
+                cexpr = CaseExpression(None, [when_min, when_max], col)
                 ce_name = scope.push_name(cexpr, str(colname))
             col.name = ce_name
         return ne
