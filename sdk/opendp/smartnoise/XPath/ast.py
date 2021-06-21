8a89d1093e3a20e84b3ce8437a5a9ca0251fd6cb
Joshua Allen <55251797+joshua-oss@users.noreply.github.com>
2021-06-20 21:43:24 -0700

    Type annotation

--- sdk/opendp/smartnoise/XPath/ast.py
+++ sdk/opendp/smartnoise/XPath/ast.py
@@ -1,40 +1,23 @@
+from typing import Union, Sequence
+
 class StringLiteral:
-    def __init__(self, value):
+    def __init__(self, value : str):
         self.value = value
 
 class NumericLiteral:
-    def __init__(self, value):
+    def __init__(self, value : Union[int, float]):
         self.value = value
 
 class Identifier:
-    def __init__(self, name):
+    def __init__(self, name : str):
         self.name = name
 
 class Attribute:
-    def __init__(self, name):
+    def __init__(self, name : str):
         self.name = name
 
-class RootSelect:
-    def __init__(self, target, condition=None):
-        self.target = target
-        self.condition = condition
-
-class ChildSelect:
-    def __init__(self, target, condition=None):
-        self.target = target
-        self.condition = condition
-
-class DescendantSelect:
-    def __init__(self, target, condition=None):
-        self.target = target
-        self.condition = condition
-
-class AllSelect:
-    def __init__(self, condition=None):
-        self.condition = condition
-
 class Statement:
-    def __init__(self, steps):
+    def __init__(self, steps : Sequence):
         self.steps = steps
 
 class Condition:
@@ -42,4 +25,24 @@ class Condition:
         self.left = left
         self.right = right
         self.operator = operator
+
+class RootSelect:
+    def __init__(self, target : Union[Identifier, Attribute], condition : Condition = None):
+        self.target = target
+        self.condition = condition
+
+class ChildSelect:
+    def __init__(self, target : Union[Identifier, Attribute], condition : Condition = None):
+        self.target = target
+        self.condition = condition
+
+class DescendantSelect:
+    def __init__(self, target : Union[Identifier, Attribute], condition : Condition = None):
+        self.target = target
+        self.condition = condition
+
+class AllSelect:
+    def __init__(self, condition : Condition = None):
+        self.condition = condition
+
 
