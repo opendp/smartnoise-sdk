eeb25a9e476a0733d31c6f9e4ab951bec6a302b8
Joshua Allen <55251797+joshua-oss@users.noreply.github.com>
2021-09-18 18:33:02 -0700

    Updates

--- tests/sdk/engines/postgres/PUMS/install.sh
+++ tests/sdk/engines/postgres/PUMS/install.sh
@@ -1,10 +1,9 @@
-# set this to location of dp-test-datasets
-DATA=../../../../../../dp-test-datasets/data/
+# set DATA to location of dp-test-datasets/data/
 
 PUMS=${DATA}PUMS_california_demographics_1000/data.csv
 PUMS_LARGE=${DATA}PUMS_california_demographics/data.csv
 tail -n +2 $PUMS > PUMS.csv
 tail -n +2 $PUMS_LARGE > PUMS_large.csv
-psql -U postgres < pums.sql
+psql -h $POSTGRES_HOST -U postgres < pums.sql
 rm PUMS.csv
 rm PUMS_large.csv
