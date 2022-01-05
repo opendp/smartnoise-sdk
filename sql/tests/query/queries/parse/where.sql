c6b220c8f0ffd85bfc02178557aa8107d255840b
victoria de sainte agathe <vdsa@sarus.tech>
2022-01-05 14:01:26 +0100

    ok

--- sql/tests/query/queries/parse/where.sql
+++ sql/tests/query/queries/parse/where.sql
@@ -6,3 +6,4 @@ SELECT Sales FROM SalesTable WHERE Receipts IS NOT NULL;
 SELECT Sales FROM SalesTable WHERE (5 + (Sales * 0.01)) > Gross;
 SELECT Sales * 99.9 AS NewSales, SUM(Sales) AS Gross FROM SalesTable HAVING (5 + (Sales * 0.01)) > Gross;
 SELECT Name FROM Invoices GROUP BY Name, Sales HAVING SalesDate = SalesDate OR Name >= 'Shipping' ORDER BY Total_Sales ASC;
+SELECT Name FROM Invoices WHERE Name LIKE 'A%';
