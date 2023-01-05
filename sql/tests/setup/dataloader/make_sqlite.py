cbe9b3adcd696e4758fc7c9214e5740f9b489339
Joshua <joshua-oss@users.noreply.github.com>
2023-01-04 20:21:18 -0800

    CI fixes

--- sql/tests/setup/dataloader/make_sqlite.py
+++ sql/tests/setup/dataloader/make_sqlite.py
@@ -1,18 +1,14 @@
 import os
 import subprocess
-import pandas as pd
 
 root_url = subprocess.check_output("git rev-parse --show-toplevel".split(" ")).decode("utf-8").strip()
 
 from sqlalchemy import create_engine, text
-from sqlalchemy import Column, Date, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 
 from .create_pums_dbs import *
 
 def make_sqlite():
-    pums_db_path = os.path.join(root_url, "datasets", "pums.db")
+    pums_db_path = os.path.join(root_url, "datasets", "PUMS.db")
     if not os.path.exists(pums_db_path):
         print('Creating PUMS in SQLite')
         sqlite_file_path = f'sqlite:///{pums_db_path}'
@@ -22,7 +18,7 @@ def make_sqlite():
             res = conn.execute(text('SELECT COUNT(*) FROM pums'))
             print(list(res))
 
-    pums_pid_db_path = os.path.join(root_url, "datasets", "pums_pid.db")
+    pums_pid_db_path = os.path.join(root_url, "datasets", "PUMS_pid.db")
     if not os.path.exists(pums_pid_db_path):
         print('Creating PUMS_pid in SQLite')
         sqlite_file_path = f'sqlite:///{pums_pid_db_path}'
@@ -32,7 +28,7 @@ def make_sqlite():
             res = conn.execute(text('SELECT COUNT(*) FROM pums'))
             print(list(res))
 
-    pums_dup_db_path = os.path.join(root_url, "datasets", "pums_dup.db")
+    pums_dup_db_path = os.path.join(root_url, "datasets", "PUMS_dup.db")
     if not os.path.exists(pums_dup_db_path):
         print('Creating PUMS_dup in SQLite')
         sqlite_file_path = f'sqlite:///{pums_dup_db_path}'
@@ -42,7 +38,7 @@ def make_sqlite():
             res = conn.execute(text('SELECT COUNT(*) FROM pums'))
             print(list(res))
 
-    pums_null_db_path = os.path.join(root_url, "datasets", "pums_null.db")
+    pums_null_db_path = os.path.join(root_url, "datasets", "PUMS_null.db")
     if not os.path.exists(pums_null_db_path):
         print('Creating PUMS_null in SQLite')
         sqlite_file_path = f'sqlite:///{pums_null_db_path}'
