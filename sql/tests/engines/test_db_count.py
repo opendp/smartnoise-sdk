91a10012bd1128dc7a6c51827dacd4f62779b6dd
Joshua <joshua-oss@users.noreply.github.com>
2023-04-23 18:56:00 -0700

    CI fix

--- sql/tests/engines/test_db_count.py
+++ sql/tests/engines/test_db_count.py
@@ -38,16 +38,18 @@ class TestDbCounts:
                 print(f"Table {dbname}.PUMS.{tablename} has {n} COUNT(age) rows in {reader.engine}")
                 assert(n > lower and n < upper)
     def test_with_two_table_meta(self, test_databases):
-        conn = test_databases.get_connection(database='PUMS_null', engine='postgres')
-        if conn is not None:
-            # pandas doesn't support multiple tables per metadata
-            table_name = conn.table_name
-            conn = conn.connection
-            query = f'SELECT COUNT(age) FROM {table_name}'
-            for meta in [two_table_meta_a, two_table_meta_b]:
-                reader = from_connection(conn, metadata=meta, privacy=privacy)
-                count_age = reader.execute(query)[1][0]
-                assert count_age > 890 and count_age < 1020
+        for engine in ['postgres', 'sqlserver']:
+            dbdataset = test_databases.get_connection(database='PUMS_null', engine=engine)
+            if dbdataset is not None:
+                # pandas doesn't support multiple tables per metadata
+                table_name = dbdataset.table_name
+                conn = dbdataset.connection
+                if table_name.upper() == 'PUMS.PUMS':
+                    query = f'SELECT COUNT(age) FROM PUMS.PUMS'
+                    for meta in [two_table_meta_a, two_table_meta_b]:
+                        reader = from_connection(conn, metadata=meta, privacy=privacy)
+                        count_age = reader.execute(query)[1][0]
+                        assert count_age > 890 and count_age < 1020
     def test_db_counts_star(self, test_databases):
         # Actual is 1000
         for dbname in ['PUMS', 'PUMS_pid', 'PUMS_large', 'PUMS_dup', 'PUMS_null']:
