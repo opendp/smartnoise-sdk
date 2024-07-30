564170da3309dc1651e0654dd299f31b4076ad3c
joshua-oss <55251797+joshua-oss@users.noreply.github.com>
2024-07-30 15:08:51 -0700

    Update for PyPi requirement (#602)
    * checkpoint
    
    * Update version support
    
    * No longer support 3.8
    
    * Don't need setup.py anymore
    
    * Update for PyPi
    
    ---------
    
    Co-authored-by: Joshua <joshua-oss@users.noreply.github.com>

--- synth/snsynth/mst/mst.py
+++ synth/snsynth/mst/mst.py
@@ -4,7 +4,7 @@ import pandas as pd
 try:
     from mbi import FactoredInference, Dataset, Domain
 except ImportError:
-    print("Please install mbi with:\n   pip install git+https://github.com/ryan112358/private-pgm.git")
+    print("Please install mbi with:\n   pip install git+https://github.com/ryan112358/private-pgm.git@01f02f17eba440f4e76c1d06fa5ee9eed0bd2bca")
     raise ImportError
 from scipy import sparse
 from disjoint_set import DisjointSet
