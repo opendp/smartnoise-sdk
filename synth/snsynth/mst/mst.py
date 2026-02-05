ee4695f2f99a570a07277e2be97ac0716274e2ca
Thomas Hall <tjh2822@gmail.com>
2026-02-05 13:46:12 -0500

    chore: fix commented out columns oversight

--- synth/snsynth/mst/mst.py
+++ synth/snsynth/mst/mst.py
@@ -143,19 +143,6 @@ class MSTSynthesizer(Synthesizer):
     def compress_domain(self, data, measurements):
         supports = {}
         new_measurements = []
-        # for Q, y, sigma, proj in measurements:
-        #     col = proj[0]
-        #     sup = y >= 3*sigma
-        #     supports[col] = sup
-        #     if supports[col].sum() == y.size:
-        #         new_measurements.append((Q, y, sigma, proj))
-        #     else:  # need to re-express measurement over the new domain
-        #         y2 = np.append(y[sup], y[~sup].sum())
-        #         I2 = np.ones(y2.size)
-        #         I2[-1] = 1.0 / np.sqrt(y.size - y2.size + 1.0)
-        #         y2[-1] /= np.sqrt(y.size - y2.size + 1.0)
-        #         I2 = sparse.diags(I2)
-        #         new_measurements.append((I2, y2, sigma, proj))
         for m in measurements:
             y = np.asarray(m.noisy_measurement)
             sigma = m.stddev
